99c0c83afe3b5f1e65f10c1b540400b95cc24a55
Saniya Ahuja <ahuja.saniya@gmail.com>
2022-04-14 12:22:14 +0530

    additions

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function image(url){
+    var animationEvent = 'webkitAnimationEnd oanimationend msAnimationEnd animationend';
+    document.getElementById("img").src=url;
+    $('#img').addClass('classname fadeUp');
+    $('#img').one(animationEvent, function(event) {
+        $('#img').removeClass('classname fadeUp');
+    });   
+}
+
+function pop(){
+    var animationEvent = 'webkitAnimationEnd oanimationend msAnimationEnd animationend';
+    document.getElementById('pop').style.display="block";
+    $('#pop').addClass('classname fadeUp');
+    $('#pop').one(animationEvent, function(event) {
+        $('#pop').removeClass('classname fadeUp');
+    });   
+}
+
